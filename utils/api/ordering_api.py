55d04119e08f6469277bf676a620cb9dee645151
liad <liadh10@gmail.com>
2023-03-08 15:09:27 +0200

    add files

--- utils/api/ordering_api.py
+++ utils/api/ordering_api.py
@@ -12,6 +12,46 @@ class OrderingAPI:
         order = requests.get(f'{self.base_url}/api/v1/orders/{order_id}', headers=self.headers)
         return order
 
+    def get_orders(self):
+        order = requests.get(f'{self.base_url}/api/v1/orders', headers=self.headers)
+        return order
+
+    def create_order(self, order_number):
+        body = {
+            "UserId": "b9e5dcdd-dae2-4b1c-a991-f74aae042814",
+            "UserName": "alice",
+            "OrderNumber": order_number,
+            "City": "Redmond",
+            "Street": "15703 NE 61st Ct",
+            "State": "WA",
+            "Country": "U.S.",
+            "ZipCode": "98052",
+            "CardNumber": "4012888888881881",
+            "CardHolderName": "Alice Smith",
+            "CardExpiration": "2024-12-31T22:00:00Z",
+            "CardSecurityNumber": "123",
+            "CardTypeId": 1,
+            "Buyer": None,
+            "RequestId": str(uuid.uuid4()),
+            "Basket": {
+                "BuyerId": "b9e5dcdd-dae2-4b1c-a991-f74aae042814",
+                "Items": [
+                    {
+                        "Id": "c1f98125-a109-4840-a751-c12a77f58dff",
+                        "ProductId": 1,
+                        "ProductName": ".NET Bot Black Hoodie",
+                        "UnitPrice": 19.5,
+                        "OldUnitPrice": 0,
+                        "Quantity": 1,
+                        "PictureUrl": "http://host.docker.internal:5202/c/api/v1/catalog/items/1/pic/"
+                    }
+                ]
+            },
+            "Id": str(uuid.uuid4()),
+            "CreationDate": "2023-03-04T14:20:24.4730559Z"
+        }
+        # order = requests.put(f'{self.base_url})
+
 
 if __name__ == '__main__':
     import pprint
