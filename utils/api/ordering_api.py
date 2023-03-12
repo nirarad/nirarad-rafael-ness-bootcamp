74db0b1d9fd83c1c87be9a072535724ac56fbff6
Ofri Shapira <ofrishapira26@gmail.com>
2023-03-12 19:29:30 +0200

    Add additional put and get requests to ordering_api.py.

--- utils/api/ordering_api.py
+++ utils/api/ordering_api.py
@@ -1,3 +1,5 @@
+import uuid
+
 import requests
 
 from bearer_tokenizer import BearerTokenizer
@@ -7,27 +9,27 @@ class OrderingAPI:
     def __init__(self, username='alice', password='Pass123%24'):
         self.base_url = 'http://localhost:5102'
         self.bearer_token = BearerTokenizer(username, password).bearer_token
-        self.headers = {"Authorization": f"Bearer {self.bearer_token}"}
+        self.headers = {"Authorization": f"Bearer {self.bearer_token}", "x-requestid": str(uuid.uuid4())}
 
     def get_order_by_id(self, order_id):
-        order = requests.get(f'{self.base_url}/api/v1/orders/{order_id}', headers=self.headers)
-        return order
+        return requests.get(f'{self.base_url}/api/v1/orders/{order_id}', headers=self.headers)
+
+    def get_orders(self):
+        return requests.get(f'{self.base_url}/api/v1/orders', headers=self.headers)
+
+    def get_card_types(self):
+        return requests.get(f'{self.base_url}/api/v1/orders/cardtypes', headers=self.headers)
 
     def cancel_order(self, order_number):
-        data = {"orderNumber": order_number}
-        print("Canceling order...")
-        response = requests.put(f'{self.base_url}/api/v1/orders/cancel', headers=self.headers)
-        return response
+        pyload = {"orderNumber": order_number}
+        return requests.put(f'{self.base_url}/api/v1/orders/cancel', headers=self.headers, json=pyload)
 
     def ship_order(self, order_number):
-        data = {"orderNumber": order_number}
-        print("Shipping order...")
-        response = requests.put(f'{self.base_url}/api/v1/orders/ship', headers=self.headers)
-        return response
+        payload = {"orderNumber": order_number}
+        return requests.put(f'{self.base_url}/api/v1/orders/cancel', json=payload,
+                            headers=self.headers)
 
 
 if __name__ == '__main__':
-    import pprint
-
-    api = OrderingAPI()
-    pprint.pprint(api.get_order_by_id(1741).json())
+    od = OrderingAPI()
+    print(od.get_card_types().json())
--- utils/db/db_utils.py
+++ utils/db/db_utils.py
@@ -32,9 +32,3 @@ class MSSQLConnector:
 
     def close(self):
         self.conn.close()
-
-
-if __name__ == '__main__':
-    import pprint
-    with MSSQLConnector() as conn:
-        pprint.pprint(conn.select_query('SELECT * from ordering.orders'))
