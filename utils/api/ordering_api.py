03d831ad254debb5f87831ed582261a031d59dff
meni rotblat <meny1717@gmail.com>
2023-03-16 00:30:02 +0200

    fix api final

--- utils/api/ordering_api.py
+++ utils/api/ordering_api.py
@@ -9,7 +9,7 @@ class OrderingAPI:
     def __init__(self, username='alice', password='Pass123%24'):
         self.base_url = 'http://localhost:5102'
         self.bearer_token = BearerTokenizer(username, password).bearer_token
-        self.headers = {"Authorization": f"Bearer {self.bearer_token}", "x-requestid": str(uuid.uuid4())}
+        self.headers = {"Authorization": f"Bearer {self.bearer_token}"}
 
     def get_order_by_id(self, order_id):
         order = requests.get(f'{self.base_url}/api/v1/orders/{order_id}', headers=self.headers)
@@ -25,6 +25,8 @@ class OrderingAPI:
         return cardtypes
 
     def cancel_order(self, order_id):
+        self.headers['Content-Type'] = 'application/json'
+        self.headers["x-requestid"] = str(uuid.uuid4())
         res = requests.put(
             f'{self.base_url}/api/v1/orders/cancel',
             json={
@@ -34,18 +36,19 @@ class OrderingAPI:
         return res.status_code
 
     def ship_order(self, order_id):
+        self.headers['Content-Type'] = 'application/json'
+        self.headers["x-requestid"] = str(uuid.uuid4())
         res = requests.put(
             f'{self.base_url}/api/v1/orders/ship',
             json={
                 "orderNumber": order_id
             }, headers=self.headers)
-        # pprint(orders.text)
         return res
-
 
 if __name__ == '__main__':
     import pprint
 
     api = OrderingAPI()
+    pprint.pprint(api.cancel_order(3090))
     # pprint.pprint(api.get_cardtypes().json())
     # print(api.cancel_order(2380))
