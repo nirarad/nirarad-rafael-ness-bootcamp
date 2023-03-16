19063ddd7fb6de57dc44e2bcb3413cf1c34be19b
Artiom <deadeadante13@gmail.com>
2023-03-16 18:14:53 +0200

    V 0.0.5 purge messages func added

--- utils/rabbitmq/rabbitmq_send.py
+++ utils/rabbitmq/rabbitmq_send.py
@@ -21,14 +21,11 @@ class RabbitMQ:
         self.connection = pika.BlockingConnection(pika.ConnectionParameters(self.host))
         self.channel = self.connection.channel()
 
-    def bind(self, queue_name, exchange_name, routing_key):
-        self.channel.queue_bind(queue_name, exchange_name, routing_key)
-
     def declare_queue(self, queue):
         self.channel.queue_declare(queue=queue)
 
-    def close(self):
-        self.connection.close()
+    def bind(self, queue_name, exchange_name, routing_key):
+        self.channel.queue_bind(queue_name, exchange_name, routing_key)
 
     def publish(self, exchange, routing_key, body):
         self.channel.basic_publish(exchange=exchange,
@@ -37,9 +34,15 @@ class RabbitMQ:
         print(f"[{routing_key}] Sent '{body}'")
         return body
 
+    def purge(self, queue):
+        self.channel.queue_purge(queue=queue)
+
     def consume(self, queue, callback):
         self.channel.basic_consume(queue=queue, on_message_callback=callback, auto_ack=True)
         self.channel.start_consuming()
+
+    def close(self):
+        self.connection.close()
 
     def __exit__(self, exc_type, exc_value, exc_traceback):
         if self.connection is not None:
