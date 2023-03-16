8575890793c03c83071043eb7fffbec1ca89b801
Artiom <deadeadante13@gmail.com>
2023-03-16 23:27:24 +0200

    V 0.0.5 Event of catalog removed to Catalog simulator

--- utils/rabbitmq/eshop_rabbitmq_events.py
+++ utils/rabbitmq/eshop_rabbitmq_events.py
@@ -5,6 +5,8 @@ import uuid
 from utils.rabbitmq.rabbitmq_send import RabbitMQ
 from dotenv import load_dotenv
 
+from utils.testcase.jsondatareader import JSONDataReader
+
 # For local running ONLY!
 load_dotenv('../../tests/DATA/.env.test')
 
@@ -31,55 +33,6 @@ def create_order(body_to_send):
         raise Exception('Message body is None.')
     # Sending message to RabbitMQ queue Ordering
     return rabbit_mq_publish(os.getenv('CREATE_ORDER_ROUTING_KEY'), body_to_send)
-
-
-def confirm_stock(order_id, x_requestid, date):
-    """
-    Name: Artsyom Sharametsieu
-    Date: 05.03.2023
-    Function Name: confirm_stock
-    Description: Function of Catalog simulator.
-                 1.Sends message to RabbitMQ queue Ordering that items in stock
-                 P.S:
-                 It is response message for request of Ordering service to check items in stock.
-    :param date: server generated date
-    :param order_id: autoincremented order id in db
-    :param x_requestid: unique id of order generated from outside
-    """
-    body = {
-        "OrderId": order_id,
-        "Id": x_requestid,
-        "CreationDate": date
-    }
-    rabbit_mq_publish(os.getenv('CONFIRM_STOCK_ROUTING_KEY'), body)
-
-
-def reject_stock(order_id, x_requestid, date):
-    """
-    Name: Artsyom Sharametsieu
-    Date: 05.03.2023
-    Function Name: reject_stock
-    Description: Function of Catalog simulator.
-                 1.Sends message to RabbitMQ queue Ordering that items not in stock
-                 P.S:
-                 It is response message for request of Ordering service to check items in stock.
-    :param date: server generated date
-    :param order_id: autoincremented order id in db
-    :param x_requestid: unique id of order generated from outside
-    """
-    load_dotenv('D:/eShopProject/rafael-ness-bootcamp/tests/DATA/.env.test')
-    body = {
-        "OrderId": order_id,
-        "OrderStockItems": [
-            {
-                "ProductId": 1,
-                "HasStock": False
-            }
-        ],
-        "Id": x_requestid,
-        "CreationDate": date
-    }
-    rabbit_mq_publish(os.getenv('REJECT_STOCK_ROUTING_KEY'), body)
 
 
 def payment_succeeded(order_id, x_requestid, date):
@@ -198,10 +151,10 @@ def status_changed_to_awaitingvalidation(order_id, x_requestid, date):
 
 
 if __name__ == '__main__':
-    # order_uuid = str(uuid.uuid4())
-    # datareader = JSONDataReader(os.getenv('ORDERS_PATH'))
-    # body = datareader.get_json_order('alice_normal_order', order_uuid)  # normal order
-    # # create_order(body)  # normal order
+    order_uuid = str(uuid.uuid4())
+    datareader = JSONDataReader(os.getenv('ORDERS_PATH'))
+    body = datareader.get_json_order('alice_normal_order', order_uuid)  # normal order
+    create_order(body)  # normal order
     # # # body = datareader.get_json_order('alice_order_empty_list', order_uuid)  # empty list
     # # # create_order(body)  # empty list
     # # # body = datareader.get_json_order('alice_order_0_quantity', order_uuid)  # 0 quantity
@@ -210,7 +163,7 @@ if __name__ == '__main__':
     # sent_body = create_order(body)  # normal order
     # d = eval(sent_body)
     # payment_succeeded(317, "2f27dd08-6360-408d-83c7-5d7cca1d44a9", d['CreationDate'])
-    payment_succeeded(319, 'f302f5a8-522a-409f-b4dd-76f9cfb7aa5e', '2023-03-13T15:43:29.2988114Z')
+    # payment_succeeded(319, 'f302f5a8-522a-409f-b4dd-76f9cfb7aa5e', '2023-03-13T15:43:29.2988114Z')
 
     # print('\n')
     # print(k)
