ac72d9c41a5f371dd2c9935c147bfbf6763ef332
Artiom <deadeadante13@gmail.com>
2023-03-18 22:18:44 +0200

    V 0.0.6 done

--- tests/test_non_functional.py
+++ tests/test_non_functional.py
@@ -23,49 +23,51 @@ class TestScalability(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         # Env of tests
-        load_dotenv('../.env.test')
-
+        load_dotenv(os.path.join(os.path.dirname(__file__), '../.env.test'))
         # Local Logger
         cls.logger = Logger('non_functional_logger', 'Logs/test_non_functional.log').logger
-
-        # Unique id generator
-        cls.order_uuid = str(uuid.uuid4())
-
         # Connection to DB
         cls.conn = MSSQLConnector()
-
         # Order Id saver
         cls.new_order_id = None
-
         # Json Data Order handler
         cls.jdata_orders = JSONDataReader('DATA/ORDERS_DATA.json')
-
         # Last order created
         cls.last_order = None
-
         # Docker manager
         cls.docker = DockerManager()
-
         # Basket Simulator
         cls.basket_sim = BasketSimulator()
-
         # Payment simulator
-        cls.payment_sim = PaymentSimulator(time_limit=300)
-
+        cls.payment_sim = None
         # Catalog simulator
-        cls.catalog_sim = CatalogSimulator(time_limit=300)
+        cls.catalog_sim = None
+        # Timeout
+        cls.timeout = 300
 
     def setUp(self) -> None:
+        # Run common containers
+        self.docker.start(os.getenv('ORDERING_BACKGROUNDTASKS_CONTAINER'))
+        self.docker.start(os.getenv('RABBITMQ_CONTAINER'))
+        self.docker.start(os.getenv('SQLDATA_CONTAINER'))
+        self.docker.stop(os.getenv('ORDERING_CONTAINER'))
+        self.docker.start(os.getenv('IDENTITY_CONTAINER'))
+        # Clean messages from previous using of RabbitMQ queues
+        with RabbitMQ() as mq:
+            mq.purge_all()
+        # Reconnect
         self.conn.__enter__()
+        self.catalog_sim = CatalogSimulator(time_limit=300)
+        self.payment_sim = PaymentSimulator(time_limit=300)
 
     @classmethod
     def tearDownClass(cls):
         cls.conn.close()
 
-    # TC017
+    # TC016
     def test_ordering_service_scalability_100_orders(self):
         """
-        TC_ID: TC017
+        TC_ID: TC016
         Name: Artsyom Sharametsieu
         Date: 05.03.2023
         Function Name: test_ordering_service_scalability_100_orders
@@ -75,19 +77,9 @@ class TestScalability(unittest.TestCase):
         """
         try:
             # Amount of orders to create and check
-            order_amount = 2
-            # Stopping ordering api and ordering.backgroundtasks containers to load Ordering queue
-            self.docker.stop('eshop/ordering.api:linux-latest')
-            self.docker.stop('eshop/ordering.backgroundtasks:linux-latest')
-
+            order_amount = 100
             # Need to know which was last before creating new order
             last_order_id = self.conn.get_last_order_record_id_in_db()
-
-            # Clean messages from previous using of RabbitMQ queue
-            with RabbitMQ() as mq:
-                mq.purge('Ordering')
-                mq.purge('Catalog')
-                mq.purge('Payment')
             # 100 orders creation loop
             for i in range(order_amount):
                 # Generating new unique x-request id
@@ -96,10 +88,8 @@ class TestScalability(unittest.TestCase):
                 message_body = self.jdata_orders.get_json_order('alice_normal_order', order_uuid)
                 # Sending message to RabbitMQ to Ordering queue to create order and getting body corrected by server
                 self.basket_sim.create_order(message_body)
-
-            # Turn on Ordering api container and Ordering BackgroundTasks container
-            self.docker.start('eshop/ordering.api:linux-latest')
-            self.docker.start('eshop/ordering.backgroundtasks:linux-latest')
+            # Turn on Ordering api container
+            self.docker.start(os.getenv('ORDERING_CONTAINER'))
             # Handler to save all created orders id to check statuses after
             order_ids = []
             # Start time of full order creation (to status 4 (paid))
@@ -120,10 +110,9 @@ class TestScalability(unittest.TestCase):
                             f'Order Id in DB -> Actual: ID {self.new_order_id}, '
                             f'Expected: New Order Id')
                         break
-                    # if 15 sec pass no sense to wait
-                    elif time.time() - start_time > 60:  # Timeout after 60 seconds
+                    # Timeout
+                    elif time.time() - start_time > self.timeout:
                         raise Exception("Order was not created")
-
             # INCLUDING SIMULATORS TO FULL ORDER CREATION FLOW
             # Threads will close by self after time (not does not affect on order creation time)
             catalog_thread = threading.Thread(target=self.catalog_sim.consume_to_confirm_stock)
@@ -133,39 +122,46 @@ class TestScalability(unittest.TestCase):
             # Check for all 100 orders status
             for order in order_ids:
                 start_time = time.time()
+                # Getting current order status
+                order_status = self.conn.get_order_status_from_db(order)
                 # WAITING UNTIL EACH ORDER WILL BE IN STATUS 4 (paid)
-                while self.conn.get_order_status_from_db(order) != 4:
-                    # Getting last order id
-                    x = self.conn.get_next_order_id(last_order_id)
-                    # if last order updated, so it will be new order
-                    if x is not None and x != last_order_id:
-                        # To pass into loger Actual
-                        order_ids.append(x)
-                        last_order_id = x
-                        self.logger.info(
-                            f'{self.test_ordering_service_scalability_100_orders.__doc__}'
-                            f'Order Id in DB -> Actual: ID {self.new_order_id}, '
-                            f'Expected: New Order Id')
-                        break
-                    elif time.time() - start_time > 400:  # Timeout after 5 minutes
+                while order_status != int(os.getenv('PAID')):
+                    # Count of time updating
+                    time_count_status_2 = 1
+                    time_count_status_3 = 1
+                    # Checking order status
+                    order_status = self.conn.get_order_status_from_db(order)
+                    # If order status updates,update time only once
+                    if order_status == int(os.getenv('AWAITINGVALIDATION')) and time_count_status_2 > 0:
+                        start_time = time.time()
+                        time_count_status_2 -= 1
+                    elif order_status == int(os.getenv('STOCKCONFIRMED')) and time_count_status_3 > 0:
+                        start_time = time.time()
+                        time_count_status_3 -= 1
+                    # Timeout
+                    elif time.time() - start_time > self.timeout:
                         raise Exception("Order status not changed,timeout.")
             # End time of full order creation (to status 4 (paid))
             process_end_time = time.time()
+            # Stopping simulators
+            self.catalog_sim.time_limit = 0
+            self.payment_sim.time_limit = 0
             # Elapsed time of full order creation (to status 4 (paid))
             elapsed_time = process_end_time - process_start_time
-            self.assertLessEqual(elapsed_time / 3600, 1)
+            self.assertLessEqual(elapsed_time / 3600, int(os.getenv('SCALABILITY_TIME')))
             self.logger.info(
-                f'{self.test_ordering_service_scalability_100_orders.__doc__} '
-                f'Order status in DB -> Actual: {elapsed_time / 3600} hours , Expected: < {1} hour/s')
+                f"{self.test_ordering_service_scalability_100_orders.__doc__} "
+                f"Order status in DB -> "
+                f"Actual: {elapsed_time / 3600} hours , "
+                f"Expected: < {int(os.getenv('SCALABILITY_TIME'))} hour/s")
         except Exception as e:
-            self.logger.exception(f"\n{self.test_ordering_service_scalability_100_orders.__doc__} Error:{e}")
+            self.logger.exception(f"\n{self.test_ordering_service_scalability_100_orders.__doc__}{e}")
             raise
 
-        # TC017
-
+    # TC017
     def test_order_api_reliability(self):
         """
-        TC_ID: TC018
+        TC_ID: TC017
         Name: Artsyom Sharametsieu
         Date: 05.03.2023
         Function Name: test_order_api_reliability
@@ -174,11 +170,6 @@ class TestScalability(unittest.TestCase):
                      3.Function sends message to create order to .
         """
         try:
-            # Clean messages from previous using of RabbitMQ queue
-            with RabbitMQ() as mq:
-                mq.purge('Ordering')
-            # Pausing ordering api container to load Ordering queue
-            self.docker.stop('eshop/ordering.api:linux-latest')
             # Handler to save all created order ids to check statuses after
             # Generating new unique x-request id
             order_uuid = str(uuid.uuid4())
@@ -189,7 +180,7 @@ class TestScalability(unittest.TestCase):
             # Sending message to RabbitMQ to Ordering queue to create order and getting body corrected by server
             self.basket_sim.create_order(message_body)
             # Running order api docker container
-            self.docker.start('eshop/ordering.api:linux-latest')
+            self.docker.start(os.getenv('ORDERING_CONTAINER'))
             # Wait until order status changed
             start_time = time.time()
             while True:
@@ -205,16 +196,16 @@ class TestScalability(unittest.TestCase):
                         f'Expected: New Order Id')
                     # Validate status order is 1
                     current_status = self.conn.get_order_status_from_db(self.new_order_id)
-                    self.assertTrue(current_status, 1)
+                    self.assertTrue(current_status, int(os.getenv('SUBMITTED')))
                     self.logger.info(
-                        f'{self.test_order_api_reliability.__doc__} '
-                        f'Order status in DB -> Actual: {current_status} , Expected: {1}')
+                        f"{self.test_order_api_reliability.__doc__} "
+                        f"Order status in DB -> Actual: {current_status} , Expected: {int(os.getenv('SUBMITTED'))}")
                     break
-                # if 60 sec pass no sense to wait
-                elif time.time() - start_time > 60:  # Timeout after 15 seconds
+                # Timeout
+                elif time.time() - start_time > self.timeout:
                     raise Exception("Time to changing status ended,order status not changed")
         except Exception as e:
-            self.logger.exception(f"\n{self.test_order_api_reliability.__doc__} Error:{e}")
+            self.logger.exception(f"\n{self.test_order_api_reliability.__doc__}{e}")
             raise
 
 
