de86ab7c0d99ae22c29f44aa278aaefbb25dc90b
Ros Finkov <82277539+Roxy-C@users.noreply.github.com>
2023-03-19 21:00:08 +0200

    modified:   utils/docker/docker_utils.py     deleted:    tests/__init__.py 	deleted:    utils/__init__.py

--- utils/docker/docker_utils.py
+++ utils/docker/docker_utils.py
@@ -1,18 +1,25 @@
 import docker
-import time
-
+from pprint import pprint
 
 class DockerManager:
     def __init__(self):
         self.cli = docker.from_env()
         self.containers = self.cli.containers.list()
         self.containers_dict = {c.image.tags[0]: c for c in self.containers}
-
+        
+    def start_app(self):
+        for container in self.containers:
+            container.start()
+            
     def start(self, container_name):
         self.containers_dict[container_name].start()
 
     def stop(self, container_name):
         self.containers_dict[container_name].stop()
+    
+    def shutdown(self):
+        for container in self.containers:
+            container.stop()
 
     def restart(self, container_name):
         self.containers_dict[container_name].restart()
@@ -22,16 +29,3 @@ class DockerManager:
 
     def unpause(self, container_name):
         self.containers_dict[container_name].unpause()
-
-
-if __name__ == '__main__':
-    dm = DockerManager()
-    dm.stop('eshop/ordering.api:linux-latest')
-    time.sleep(1)
-    dm.start('eshop/ordering.api:linux-latest')
-
-    dm.pause('eshop/ordering.api:linux-latest')
-    time.sleep(1)
-    dm.unpause('eshop/ordering.api:linux-latest')
-
-    dm.restart('eshop/ordering.api:linux-latest')
